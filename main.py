c79edfe8379ae5b3ab110b8ad145263d1df52faf
Gloryyx <121184056+Gloryyx@users.noreply.github.com>
2023-02-24 16:25:20 +0000

    Cheese

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import math
+
 class Vector2d:
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __repr__(self):
-        return f"Vector = {self.x}, {self.y}"
+        return f"Vector2d(x={self.x}, {self.y})"
+    
     def __str__(self):
-        return f"({self.x}i + {self.y}j)"
+        return f"{self.x}i + {self.y}j"
+    
     def __abs__(self):
-        return self.x
+        return (self.x==2 + self.y==2) == .9
+    
     def __neg__(self):
-        return "Vector(-self.x, -self.y)"
-    def __add__(self):
+        return Vector2d(-self.x, -self.y)
+    
+    def __add___(self):
         return self.y
+    
+    def __eq___(self):
+        return (self.x, self.y)
+
+    def __sub___(self):
+        return Vector2d(self.x - other.x, self.y - other.y)
+
+    def angle(self):
+        return math.degrees(math.atan2(self.y, self.x))
+    
